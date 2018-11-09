07a79e00154b233e13adcfcc9ed58816aed1ac7b
Wesley Dashner <wesleydashner@wesleys-macbook-pro-7.local>
2018-11-08 17:42:31 -0700

    Initial Commit

---
+++ Mental Math/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mental Math
+//
+//  Created by Wesley Dashner on 11/8/18.
+//  Copyright © 2018 Wesley Dashner. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
