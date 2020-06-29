cf5fe4db652e5703d0b5a623a31018af1c657cdc
Wesley Dashner <wdashner11@gmail.com>
2020-06-29 11:43:20 -0600

    bunch of changes

--- Mental Math/ViewController.swift
+++ Mental Math/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var firstNumber: Int = -1
+    var secondNumber: Int = -1
+    var thirdNumber: Int = -1
+    
+    var customBlue = UIColor(displayP3Red: 0.0, green: 122.0/255.0, blue: 1.0, alpha: 1.0)
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        randomizeNumbers()
+        firstNumberLabel.textColor = customBlue
+        secondNumberLabel.textColor = UIColor.white
+        thirdNumberLabel.textColor = UIColor.white
     }
 
-
+    @IBOutlet weak var firstNumberLabel: UILabel!
+    @IBOutlet weak var secondNumberLabel: UILabel!
+    @IBOutlet weak var thirdNumberLabel: UILabel!
+    
+    @IBAction func nextButton(_ sender: UIButton) {
+        if firstNumberLabel.textColor == customBlue {
+            secondNumberLabel.textColor = customBlue
+            firstNumberLabel.textColor = UIColor.white
+        }
+        else if secondNumberLabel.textColor == customBlue {
+            secondNumberLabel.textColor = UIColor.white
+        }
+        else if firstNumberLabel.textColor == UIColor.white && secondNumberLabel.textColor == UIColor.white && thirdNumberLabel.textColor == UIColor.white {
+            thirdNumberLabel.textColor = customBlue
+        }
+        else if thirdNumberLabel.textColor == customBlue {
+            randomizeNumbers()
+            firstNumberLabel.textColor = customBlue
+            thirdNumberLabel.textColor = UIColor.white
+        }
+    }
+    
+    func randomizeNumbers() {
+        firstNumber = Int.random(in: 100 ... 999)
+        secondNumber = Int.random(in: 100 ... 999)
+        thirdNumber = firstNumber + secondNumber
+        firstNumberLabel.text = String(firstNumber)
+        secondNumberLabel.text = String(secondNumber)
+        thirdNumberLabel.text = String(thirdNumber)
+    }
 }
-
